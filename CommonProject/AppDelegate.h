8b36ede66f98f358f39b69e175741c801983c588
曾祥宪 <zoekebi@126.com>
2017-10-26 10:32:49 +0800

    second comit

---
+++ CommonProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommonProject
+//
+//  Created by 曾祥宪 on 2017/10/26.
+//  Copyright © 2017年 曾祥宪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CommonProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CommonProject
+//
+//  Created by 曾祥宪 on 2017/10/26.
+//  Copyright © 2017年 曾祥宪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
